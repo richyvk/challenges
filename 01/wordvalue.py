3db317a62369a3cbba8867e64e58332fed7ba216
Richard Vankoningsveld <richyvk@gmail.com>
2017-03-11 22:03:51 +1000

    Add calc_word_value function

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,5 +1,6 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words(dict_file=DICTIONARY):
     """ (txt file) -> list of str
 
@@ -12,3 +13,17 @@ def load_words(dict_file=DICTIONARY):
         words_list = f.read().splitlines()
 
     return words_list
+
+
+def calc_word_value(word):
+    """ (str) -> int
+
+    Return the scrabble score for word.
+
+    >>> calc_word_value('foo')
+    6
+    """
+    score = sum([LETTER_SCORES.get(letter, 0)
+                for letter in word.upper()])
+
+    return score
